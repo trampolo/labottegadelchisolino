314bd59b31298f24b8fe57f2ec2ac590aefda371
trampolo <rrotta@gmail.com>
2011-11-02 12:14:24 +0100

    Init master

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Labottegadelchisolino::Application.config.secret_token = '70b095aeaaf996262c9c0c93ea4f2c46dbc73c65240a07b424b071c78fdded1872241ff65bf126c3da43d97ff5d17ed3e421f512bb3f33cade47bf4cb8356ef4'
