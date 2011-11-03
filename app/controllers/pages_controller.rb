56687dcf373de2c91d6b8833801ab88656819b68
trampolo <rrotta@gmail.com>
2011-11-03 12:19:07 +0100

    Done with styling

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,19 @@
+class PagesController < ApplicationController
+  
+  def home
+    @title = "Home"
+  end
+
+  def about
+    @title = "Chi siamo"
+  end
+
+  def contact
+    @title = "Contatti"
+  end
+
+  def where
+    @title = "Dove siamo"
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Labottegadelchisolino::Application.routes.draw do
+  
+  match '/',        :to => 'pages#home'
+  match '/about',   :to => 'pages#about'
+  match '/contact', :to => 'pages#contact'
+  match '/where',   :to => 'pages#where'
+  
+  root :to => 'pages#home'
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
